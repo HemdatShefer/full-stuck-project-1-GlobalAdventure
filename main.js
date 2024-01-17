98cf53f4d89c7a0a860720d88aa207532cd92baf
HemdatShefer <hemdatse@gmail.com>
2024-01-17 23:35:13 +0200

    Fix CSS and update navigation in main.js

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 class MyHeader extends HTMLElement{
     connectedCallback(){
         this.innerHTML =
-      '<header>'+
+      '    <header>'+
       '<div class="header-inner">'+
-         
+          '<h1 class="name">'+
+             ' <img src="images/logo .png" alt="Logo" style="border-radius: 100%; width:80px;margin-right: 5px;"> Global Adventure'+
+         ' </h1>'+
           '<nav class="dropdownmenu">'+
             '<ul>'+
-            '<h1 class="name">'+
-            ' <img src="images/logo .png" alt="Logo" style="border-radius: 100%; width:80px;margin-right: 5px;"> Global Adventure'+
-            ' </h1>'+
                 '  <li><a href="index.html">Home </a></li>'+
                   '<li><a href="about.html">About Us</a></li>'+
                   '<li><a href="contact.html">Contact</a></li>'+
-                  '<li> <a>Destinations</a>'+
+                  '<li> <a href="destinations.html">Destinations</a>'+
                       '<ul id="submenu">'+
                           '<li><a href="italy.html">italy</a></li>'+
                           '<li><a href="greece.html">Greece</a></li>'+
-                          '<li><a href="israel.html">Israel</a></li>'+
+                          '<li><a href="USA.html">USA</a></li>'+
                       '</ul>'+
                   '</li>'+
             ' </ul> '+
